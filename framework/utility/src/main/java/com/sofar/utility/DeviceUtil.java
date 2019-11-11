c743d2395a0427955421c3e361ac92fb860d834b
sufan <sofarsogood001@163.com>
2019-11-11 18:49:03 +0800

    添加常用的util工具类

--- framework/utility/src/main/java/com/sofar/utility/DeviceUtil.java
+++ framework/utility/src/main/java/com/sofar/utility/DeviceUtil.java
@@ -1,12 +1,15 @@
 package com.sofar.utility;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
 import androidx.annotation.NonNull;
 
@@ -14,51 +17,6 @@ import androidx.annotation.NonNull;
  * 获取设备信息
  */
 public class DeviceUtil {
-
-  /**
-   * 获取屏幕宽度px
-   */
-  public static int getMetricsWidth(@NonNull Context context) {
-    DisplayMetrics dm = context.getResources().getDisplayMetrics();
-    int screenWidth = dm.widthPixels;
-    return screenWidth;
-  }
-
-
-  /**
-   * 获取屏幕高度px
-   */
-  public static int getMetricsHeight(@NonNull Context context) {
-    DisplayMetrics dm = context.getResources().getDisplayMetrics();
-    int screenHeight = dm.heightPixels;
-    return screenHeight;
-  }
-
-  /**
-   * 获取手机屏幕dpi
-   * mdpi{120~160}
-   * hdpi{160~240}
-   * xhdpi{240~320}
-   * xxhdpi{320~480}
-   * xxxhdpi{480~640}
-   * <p>
-   * 计算公式
-   * px = density * dp;
-   * dp = px / density;
-   * density = dpi / 160;
-   */
-  public static int getMetricsDensityDpi(@NonNull Context context) {
-    DisplayMetrics dm = context.getResources().getDisplayMetrics();
-    return dm.densityDpi;
-  }
-
-  /**
-   * dp和px的转化比例
-   */
-  public static float getMetricsDensity(@NonNull Context context) {
-    DisplayMetrics dm = context.getResources().getDisplayMetrics();
-    return dm.density;
-  }
 
   /**
    * 获取手机型号，如MI 8
@@ -122,6 +80,72 @@ public class DeviceUtil {
   private static String getUUid(@NonNull Context context) {
     String uuid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
     return uuid;
+  }
+
+  /**
+   * 获取屏幕宽度px
+   */
+  public static int getMetricsWidth(@NonNull Context context) {
+    DisplayMetrics dm = context.getResources().getDisplayMetrics();
+    int screenWidth = dm.widthPixels;
+    return screenWidth;
+  }
+
+
+  /**
+   * 获取屏幕高度px
+   */
+  public static int getMetricsHeight(@NonNull Context context) {
+    DisplayMetrics dm = context.getResources().getDisplayMetrics();
+    int screenHeight = dm.heightPixels;
+    return screenHeight;
+  }
+
+  /**
+   * 获取手机屏幕dpi
+   * mdpi{120~160}
+   * hdpi{160~240}
+   * xhdpi{240~320}
+   * xxhdpi{320~480}
+   * xxxhdpi{480~640}
+   * <p>
+   * 计算公式
+   * px = density * dp;
+   * dp = px / density;
+   * density = dpi / 160;
+   */
+  public static int getMetricsDensityDpi(@NonNull Context context) {
+    DisplayMetrics dm = context.getResources().getDisplayMetrics();
+    return dm.densityDpi;
+  }
+
+  /**
+   * dp和px的转化比例
+   */
+  public static float getMetricsDensity(@NonNull Context context) {
+    DisplayMetrics dm = context.getResources().getDisplayMetrics();
+    return dm.density;
+  }
+
+
+  public static int dp2px(@NonNull Context context, float dpVal) {
+    int value = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal, context.getResources().getDisplayMetrics());
+    return value;
+  }
+
+  public static int sp2px(@NonNull Context context, float spVal) {
+    int value = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spVal, context.getResources().getDisplayMetrics());
+    return value;
+  }
+
+  public static float px2dp(@NonNull Context context, float pxVal) {
+    float scale = context.getResources().getDisplayMetrics().density;
+    return (pxVal / scale);
+  }
+
+  public static float px2sp(@NonNull Context context, float pxVal) {
+    float scale = context.getResources().getDisplayMetrics().scaledDensity;
+    return (pxVal / scale);
   }
 
 }
