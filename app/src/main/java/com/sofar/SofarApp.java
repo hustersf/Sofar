21af5d685a15a25461cac70a6b0dd1d93b8b0ebf
sufan <sofarsogood001@163.com>
2020-08-31 13:06:04 +0800

    rxjava 在debug环境下将吃掉的异常抛出来，方便问题定位

--- app/src/main/java/com/sofar/SofarApp.java
+++ app/src/main/java/com/sofar/SofarApp.java
@@ -1,10 +1,17 @@
 package com.sofar;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.sofar.base.exception.SofarErrorConsumer;
 import com.sofar.base.location.LocationProvider;
 import com.sofar.fun.FunConfig;
 import com.sofar.skin.core.Skin;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import io.reactivex.plugins.RxJavaPlugins;
 
 public class SofarApp extends Application {
 
@@ -17,5 +24,17 @@ public class SofarApp extends Application {
     LocationProvider.getInstance().init(this);
 
     FunConfig.init(this);
+
+    RxJavaPlugins.setErrorHandler(new SofarErrorConsumer() {
+      @Override
+      public void accept(Throwable t) throws Exception {
+        super.accept(t);
+        StringWriter sw = new StringWriter(256);
+        PrintWriter pw = new PrintWriter(sw, false);
+        t.printStackTrace(pw);
+        pw.flush();
+        Log.d("rx_error", sw.toString());
+      }
+    });
   }
 }
