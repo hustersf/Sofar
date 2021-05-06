a9f6d092ab3e2910b64093d9304b39345bd9b5e6
sufan <sofarsogood001@163.com>
2021-05-06 21:01:28 +0800

    优化 grid 间距逻辑

--- framework/widget/src/main/java/com/sofar/widget/recycler/GridMarginItemDecoration.java
+++ framework/widget/src/main/java/com/sofar/widget/recycler/GridMarginItemDecoration.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * 处理 {@link GridLayoutManager} 的分割，控制分割的大小.
+ * 方向为 {@link RecyclerView#VERTICAL}
  */
 public class GridMarginItemDecoration extends RecyclerView.ItemDecoration {
 
@@ -15,6 +16,8 @@ public class GridMarginItemDecoration extends RecyclerView.ItemDecoration {
   private final int mItemSpace;
   private final int mLeftSpace;
   private final int mRightSpace;
+
+  private boolean mEnableTopSpace;
 
   public GridMarginItemDecoration(int mSpanCount, int mItemSpace) {
     this(mSpanCount, mItemSpace, 0, 0);
@@ -27,12 +30,17 @@ public class GridMarginItemDecoration extends RecyclerView.ItemDecoration {
     this.mRightSpace = mRightSpace;
   }
 
+  public void setEnableTopSpace(boolean enableTopSpace) {
+    mEnableTopSpace = enableTopSpace;
+  }
+
   @Override
-  public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+  public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+    RecyclerView.State state) {
     int position = parent.getChildAdapterPosition(view);
     int column = position % mSpanCount;
-    outRect.top = 0;
-    outRect.bottom = mItemSpace;
+    outRect.top = mEnableTopSpace ? mItemSpace : 0;
+    outRect.bottom = mEnableTopSpace ? 0 : mItemSpace;
     if (column == 0) {
       outRect.left = mLeftSpace;
       outRect.right = mItemSpace / 2;
