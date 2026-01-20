c343f54818ee009f92db015e2e02e2fe05a1364e
sufan <sofarsogood001@163.com>
2026-01-20 12:16:55 +0800

    [网络框架2]-健壮性优化：处理云端 errorCode == 0 但不返回 data 的情况

--- framework/network2/src/main/java/com/sofar/network2/api/ApiException.kt
+++ framework/network2/src/main/java/com/sofar/network2/api/ApiException.kt
@@ -5,15 +5,26 @@ class ApiException(
   val errorMsg: String,
 ) : RuntimeException("API Business Failure[$errorCode]: $errorMsg") {
 
+  companion object {
+    /** 服务器返回成功但 data 字段缺失 */
+    const val CODE_EMPTY_DATA = -1
+  }
+
   // 增加一些快捷判断，方便业务方使用
-  fun isTokenExpired(): Boolean = errorCode == 401
-  fun isParamError(): Boolean = errorCode == 400
 }
 
-fun <T> Result<ApiResponse<T>>.unwrap(): Result<T> {
+inline fun <reified T> Result<ApiResponse<T>>.unwrap(): Result<T> {
   return this.mapCatching { response ->
     if (response.errorCode == 0) {
-      response.data
+      val data = response.data
+      when {
+        data != null -> data
+        Unit is T -> Unit as T
+        else -> throw ApiException(
+          ApiException.CODE_EMPTY_DATA,
+          "Response data is null, but expected ${T::class.simpleName}"
+        )
+      }
     } else {
       // 这里 throw 会被 Result.map 捕获，从而转为 Result.failure
       throw ApiException(response.errorCode, response.errorMsg)
@@ -21,7 +32,7 @@ fun <T> Result<ApiResponse<T>>.unwrap(): Result<T> {
   }
 }
 
-suspend inline fun <S, T> S.execute(
+suspend inline fun <S, reified T> S.execute(
   crossinline call: suspend S.() -> Result<ApiResponse<T>>,
 ): Result<T> {
   return this.call().unwrap()
